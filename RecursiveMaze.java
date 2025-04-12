85a665e956147ced55cc7a6f915c7b711af97c6a
jamijyaaa <eshmurzaevajamilya@gmail.com>
2025-04-12 18:34:31 +0600

    Добавила генерацию лабиринта с помощью рекурсии

--- RecursiveMaze.java
+++ RecursiveMaze.java
@@ -31,3 +31,34 @@ class Maze {
         }
     }
 }
+public void generate() {
+    for (int y = 0; y < height; y++)
+        for (int x = 0; x < width; x++)
+            grid[y][x].isWall = true;
+
+    generateRecursive(1, 1);
+}
+
+private void generateRecursive(int x, int y) {
+    grid[y][x].isWall = false;
+    int[] dirs = {0, 1, 2, 3};
+    shuffleArray(dirs);
+
+    for (int dir : dirs) {
+        int dx = 0, dy = 0;
+        switch (dir) {
+            case 0 -> dy = -2;
+            case 1 -> dy = 2;
+            case 2 -> dx = -2;
+            case 3 -> dx = 2;
+        }
+
+        int nx = x + dx;
+        int ny = y + dy;
+
+        if (inBounds(nx, ny) && grid[ny][nx].isWall) {
+            grid[y + dy / 2][x + dx / 2].isWall = false;
+            generateRecursive(nx, ny);
+        }
+    }
+}
